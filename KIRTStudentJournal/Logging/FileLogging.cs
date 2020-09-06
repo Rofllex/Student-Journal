58afd05ed355cbad636e2ce73d907dc3007f19ce
Rofllex <41532601+Rofllex@users.noreply.github.com>
2020-09-06 19:08:43 +0500

    Вывод путя до текстового файла.

--- KIRTStudentJournal/Logging/FileLogging.cs
+++ KIRTStudentJournal/Logging/FileLogging.cs
@@ -8,15 +8,15 @@ namespace KIRTStudentJournal.Logging
     {
         public const string LOGS_DIRECTORY_NAME = "logs";
         private const string _LOG_FILE_NAME_FORMAT = "dd.MM.yyyy-HH.mm.ss";
-        public static StreamWriter CreateLogFile()
+        public static StreamWriter CreateLogFile(out string logsFilePath)
         {
             var asm = Assembly.GetExecutingAssembly();
             var executingAssemblyDirectoryPath = Path.GetDirectoryName(asm.Location);
-            var filePath = Path.Combine(executingAssemblyDirectoryPath, LOGS_DIRECTORY_NAME, $"log-{DateTime.Now.ToString(_LOG_FILE_NAME_FORMAT)}.txt");
-            return File.CreateText(filePath);
+            logsFilePath = Path.Combine(executingAssemblyDirectoryPath, LOGS_DIRECTORY_NAME, $"log-{DateTime.Now.ToString(_LOG_FILE_NAME_FORMAT)}.txt");
+            return File.CreateText(logsFilePath);
         }
 
-        public static StreamWriterLogger CreateLogger() => new StreamWriterLogger(CreateLogFile());
+        public static StreamWriterLogger CreateLogger(out string logsFilePath) => new StreamWriterLogger(CreateLogFile(out logsFilePath));
         
     }
 }
