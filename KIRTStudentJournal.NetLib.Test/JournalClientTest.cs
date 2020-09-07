b3e5ce4687e8c39030b40e81df935c76c511e3b7
Rofllex <41532601+Rofllex@users.noreply.github.com>
2020-09-08 02:51:08 +0500

    Тесты тесты тесты

--- KIRTStudentJournal.NetLib.Test/JournalClientTest.cs
+++ KIRTStudentJournal.NetLib.Test/JournalClientTest.cs
@@ -23,6 +23,19 @@ namespace KIRTStudentJournal.NetLib.Test
             Uri uri = MockClientBase.BuildUriTest(new Uri("http://localhost:5001/"), "");
             Assert.Equal("http://localhost:5001/", uri.ToString());
         }
+
+        [Fact]
+        public async void SuccessAuth()
+        {
+            var journal = await JournalClient.SignInAsync(new Uri("https://localhost:5001"), "12345", "1");
+            Assert.Equal("Admin", journal.Role);
+        } 
+        [Fact]
+        public void FaillureAuth()
+        {
+
+        }
+        
     }
 
     public class MockClientBase : ClientBase
@@ -33,5 +46,10 @@ namespace KIRTStudentJournal.NetLib.Test
         }
 
         public static Uri BuildUriTest(Uri baseUri, string method, params string[] args) => ClientBase.BuildUri(baseUri, method, args);
+
+        public override void RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
