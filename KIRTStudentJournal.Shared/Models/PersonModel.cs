ba45a5ceef4775ebfab5eed944f67c4c9a88a3af
0xFFour <null>
2020-09-19 19:16:51 +0500

    Перенос модели Person в Shared проект

---
+++ KIRTStudentJournal.Shared/Models/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KIRTStudentJournal.Shared.Models
+{
+    public class PersonModel
+    {
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+        [JsonProperty("lastName")]
+            
+        public string LastName { get; set; }
+        [JsonProperty("patronymic")]
+        public string Patronymic { get; set; }
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+    
+        public PersonModel(string firstName, string lastName, string patronymic, string phoneNumber = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Patronymic = patronymic;
+            PhoneNumber = phoneNumber;
+        }
+
+        public PersonModel()
+        {
+        }
+    }
+}
