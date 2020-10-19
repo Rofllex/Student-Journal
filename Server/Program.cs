2112956cc7aca5bebb11a31cab05ba9024cc75a2
0xFFour <null>
2020-10-19 23:03:33 +0500

    Вынос инициализации в отдельные методы

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,6 +14,9 @@ namespace Server
     {
         public static void Main(string[] args)
         {
+            InitializeLogging();
+            InitializeDatabase();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -23,5 +26,14 @@ namespace Server
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void InitializeDatabase()
+        {
+            Database.JournalDbContext.SetConnectionString("Server=localhost; Database=studentjournal; Uid=root; Pwd=root");
+            using (Database.JournalDbContext dbContext = new Database.JournalDbContext())
+                dbContext.Database.EnsureCreated();
+        }
+
+        private static void InitializeLogging() => Logging.Logger.SetInstance(new Logging.ConsoleLogger());
     }
 }
