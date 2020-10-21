66836a0bf87b51e99a480345bf60d90f84a5c32d
Maxim <1@1.er>
2020-10-21 23:30:44 +0500

    sync

--- Server/Database/JournalDbContext.cs
+++ Server/Database/JournalDbContext.cs
@@ -1,22 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.Xml;
 
 using Microsoft.EntityFrameworkCore;
 
+using Org.BouncyCastle.Asn1.Mozilla;
+
 namespace Server.Database
 {
+    /// <summary>
+    /// Контекст для подключения к базе данных.
+    /// Прежде чем начать использовать необходимо вызвать метод <see cref="SetConnectionString(string)"/> для установки строки подключения.
+    /// Данный экземпляр характеризует только одно подключение к базе данных. 
+    /// </summary>
     public class JournalDbContext : DbContext
     {
         public virtual DbSet<User> Users { get; set; }
-        
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<UserToRole> UsersToRoles { get; set; }
+
         public JournalDbContext()
         {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySQL(_connectionString);
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserToRole>()
+                .HasKey(u => new { u.UserId, u.RoleId });
+            modelBuilder.Entity<UserToRole>()
+                .HasOne(u => u.User)
+                .WithMany(u => u.UserRole)
+                .HasForeignKey(k => k.UserId);
+            modelBuilder.Entity<UserToRole>()
+                .HasOne(r => r.Role)
+                .WithMany(r => r.UserToRoles)
+                .HasForeignKey(k => k.RoleId);
+        }
+
 
         private static string _connectionString = string.Empty;
         public static void SetConnectionString(string connectionString)
@@ -25,6 +49,9 @@ namespace Server.Database
         }
     }
 
+    /// <summary>
+    /// Модель пользователя в БД.
+    /// </summary>
     public class User
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -59,42 +86,69 @@ namespace Server.Database
         [MaxLength(length: 64)]
         public string PasswordHash { get; set; }
 
-        [Required]
-        public virtual UserRole Role { get; set; }
-    
+        /// <summary>
+        /// Множество ролей.
+        /// </summary>
+        public List<UserToRole> UserRole { get; set; } = new List<UserToRole>();
+
         public User()
         {
         }
 
-        public User(string firstName, string surname, string lastName, string login, string passwordHash = "", UserRole role = UserRole.Student)
+        public User(string firstName, string surname, string lastName, string login, string passwordHash = "")
         {
             FirstName = firstName;
             Surname = surname;
             LastName = lastName;
             Login = login;
             PasswordHash = passwordHash;
+        }
+    }
+
+    /// <summary>
+    /// Дерьмо случается эта хуйня не поддерживает по умолчанию многие-ко-многим 
+    /// </summary>
+    public class UserToRole
+    {
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+    
+        public UserToRole()
+        {
+        }
+
+        public UserToRole(User user, Role role)
+        {
+            User = user;
             Role = role;
         }
     }
 
-    [Flags]
-    public enum UserRole
+    /// <summary>
+    /// Роль пользователя
+    /// </summary>
+    public class Role
     {
-        /// <summary>
-        /// Студент
-        /// </summary>
-        Student = 1,
-        /// <summary>
-        /// Преподаватель
-        /// </summary>
-        Teacher = 2,
-        /// <summary>
-        /// Куратор группы.
-        /// </summary>
-        Curator = 4,
-        /// <summary>
-        /// Администратор системы
-        /// </summary>
-        SysAdmin = 8,
+        public static IReadOnlyCollection<string> DefaultRoles = new string[] { "Student", "Teacher", "Curator", "SysAdmin" };
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required(AllowEmptyStrings = false), MinLength(3)]
+        public string Name { get; set; }
+
+        public List<UserToRole> UserToRoles { get; set; } = new List<UserToRole>();
+
+        public Role()
+        {
+        }
+
+        public Role(string name)
+        {
+            Name = name;
+        }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,23 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Server.Database;
 
 namespace Server
 {
     public class Program
     {
+        public static JToken Config { get; private set; }
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             InitializeLogging();
+            LoadConfig();
             InitializeDatabase();
             
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logging.Logger.Instance.Fatal("Необработанное исключение");
+            Logging.Logger.Instance.Fatal((Exception)e.ExceptionObject);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -29,9 +45,39 @@ namespace Server
 
         private static void InitializeDatabase()
         {
-            Database.JournalDbContext.SetConnectionString("Server=localhost; Database=studentjournal; Uid=root; Pwd=root");
-            using (Database.JournalDbContext dbContext = new Database.JournalDbContext())
-                dbContext.Database.EnsureCreated();
+            string connectionString = Config["db"]["connection_string"].ToObject<string>();
+            JournalDbContext.SetConnectionString(connectionString);
+            using (JournalDbContext dbContext = new Database.JournalDbContext())
+            {
+                IEnumerable<string> rolesToCreate;
+
+                if (dbContext.Database.EnsureCreated())
+                    rolesToCreate = Role.DefaultRoles;
+                else
+                    rolesToCreate = Role.DefaultRoles.Except(dbContext.Roles.ToList().ConvertAll(u => u.Name));
+                using (IEnumerator<string> enumerator = rolesToCreate.GetEnumerator())
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        do
+                        {
+                            string roleName = enumerator.Current;
+                            Role role = new Role(roleName);
+                            dbContext.Roles.Add(role);
+                        } while (enumerator.MoveNext());
+                        dbContext.SaveChanges();
+                    }
+                }
+            }
+            Logging.Logger.Instance.Info("База данных инициализирована");
+        }
+
+        private static void LoadConfig()
+        {
+            string jsonString;
+            using (StreamReader reader = File.OpenText("Config/config.json"))
+                jsonString = reader.ReadToEnd();
+            Config = JsonConvert.DeserializeObject<JToken>(jsonString);
         }
 
         private static void InitializeLogging() => Logging.Logger.SetInstance(new Logging.ConsoleLogger());
