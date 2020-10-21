2901c78eb82b2fb57b3d48d7326fd073b7f4e77f
Maxim <1@1.er>
2020-10-21 23:37:00 +0500

    Вынос классов из одного .cs файла.

---
+++ Server/Database/Role.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Database
+{
+    /// <summary>
+    /// Роль пользователя
+    /// </summary>
+    public class Role
+    {
+        /// <summary>
+        /// Роли по умолчанию.
+        /// </summary>
+        public static IReadOnlyCollection<string> DefaultRoles = new string[] { "Student", "Teacher", "Curator", "SysAdmin" };
+
+        /// <summary>
+        /// Идентификатор роли.
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// Название роли
+        /// </summary>
+        [Required(AllowEmptyStrings = false), MinLength(3)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Связь многие-ко-многим с пользователями.
+        /// </summary>
+        public List<UserToRole> UserToRoles { get; set; } = new List<UserToRole>();
+
+        public Role()
+        {
+        }
+
+        public Role(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Server/Database/User.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Database
+{
+    /// <summary>
+    /// Модель пользователя в БД.
+    /// </summary>
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+        
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        public string Surname { get; set; }
+        
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Логин
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        public string Login { get; set; }
+        
+        /// <summary>
+        /// Хэш пароля
+        /// </summary>
+        [MaxLength(length: 64)]
+        public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// Связь многие-ко-многим с ролями.
+        /// </summary>
+        public List<UserToRole> UserRole { get; set; } = new List<UserToRole>();
+
+        public User()
+        {
+        }
+
+        public User(string firstName, string surname, string lastName, string login, string passwordHash = "")
+        {
+            FirstName = firstName;
+            Surname = surname;
+            LastName = lastName;
+            Login = login;
+            PasswordHash = passwordHash;
+        }
+    }
+}
