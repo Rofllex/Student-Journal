48703b6bdb160aa90712d58cabb89f7e91c7bdfc
Maxim <1@1.er>
2020-10-25 01:16:55 +0500

    Создание класса опций авторизации

---
+++ Server/Security/AuthOptions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Security
+{
+    public static class AuthOptions
+    {
+        /// <summary>
+        /// Издатель токена
+        /// </summary>
+        public static string ISSUER => "StudentJournal-yQdfWlvYso";
+
+        /// <summary>
+        /// Потребитель токена
+        /// </summary>
+        public static string AUDIENCE => "StudentJournalClient-JoocuCa12Z";
+        
+        /// <summary>
+        /// Время жизни токена
+        /// </summary>
+        public static readonly TimeSpan LIFETIME = new TimeSpan(1, 0, 0);
+
+
+        private const string KEY = "testkey";
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+}
