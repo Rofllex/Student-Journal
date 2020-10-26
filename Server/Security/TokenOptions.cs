d891d4f32e002b1dce3f680964105e4feef9dd84
Maxim <1@1.er>
2020-10-27 01:29:53 +0500

    Добавление настроек токена в отдельный класс.

---
+++ Server/Security/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Security
+{
+    public static class TokenOptions
+    {
+        /// <summary>
+        /// Тип роли
+        /// </summary>
+        public const string ROLE_TYPE = "_role";
+        /// <summary>
+        /// Тип имени
+        /// </summary>
+        public const string NAME_TYPE = "_login";
+    }
+}
