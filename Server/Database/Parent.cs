d26e17d3929717a98a11fb713829419ab13b7610
Maxim <1@1.er>
2020-10-27 01:27:21 +0500

    Добавление сущности родителя.

---
+++ Server/Database/Parent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Server.Database
+{
+    /// <summary>
+    /// Класс родителя.
+    /// </summary>
+    public class Parent : User
+    {
+        public List<Student> Childs { get; set; } = new List<Student>();
+    }
+}
