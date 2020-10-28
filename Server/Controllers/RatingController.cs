961f73d867e325ec671c8e736da6b60f7fa7fd1e
Maxim <1@1.er>
2020-10-28 22:40:49 +0500

    Создание контроллера работы с оценками

---
+++ Server/Controllers/RatingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Server.Database;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route(Infrastructure.ApiControllersRouting.API_CONTROLLER_DEFAULT_ROUTE)]
+    [Authorize]
+    public class RatingController : Controller
+    {
+        private readonly JournalDbContext dbContext;
+        public RatingController()
+        {
+            dbContext = JournalDbContext.CreateContext();
+        }
+
+        
+        
+    }
+}
