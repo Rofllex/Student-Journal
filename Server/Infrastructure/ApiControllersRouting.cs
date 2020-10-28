f9385d9469f2253fdfbb855804a94e51c9d7dd7c
Maxim <1@1.er>
2020-10-28 22:39:30 +0500

    Добавление константы роутинга контроллера по умолчанию

---
+++ Server/Infrastructure/ApiControllersRouting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Infrastructure
+{
+    public static class ApiControllersRouting
+    {
+        public const string API_CONTROLLER_DEFAULT_ROUTE = "api/[controller]/[action]";
+    }
+}
