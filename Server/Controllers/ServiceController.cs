21dde6748e6604b070eace8a3bb49d5007dbcfb7
Maxim <1@1.er>
2020-12-05 01:28:26 +0500

    Создание сервисного контроллера.

---
+++ Server/Controllers/ServiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Journal.Server.Infrastructure;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journal.Server.Controllers
+{
+    [ApiController
+        , Route(ApiControllersRouting.API_CONTROLLER_DEFAULT_ROUTE)]
+    public class ServiceController : Controller
+    {
+        public IActionResult GetStats()
+        {
+            DateTime startTime = _currentProcess.StartTime;
+            return Json(new
+            {
+                startTime,
+                workTime = DateTime.Now - startTime
+            });
+        }
+
+        private static Process _currentProcess = Process.GetCurrentProcess();
+    }
+}
