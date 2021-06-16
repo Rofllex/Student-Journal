ed48af7f308d874a2812d655438cf763402d25d2
Maxim <roflex.godgame@gmail.com>
2021-06-17 01:23:46 +0500

    Рефакторинг

--- Server/Database/JournalDbContext.cs
+++ Server/Database/JournalDbContext.cs
@@ -5,13 +5,27 @@ using Microsoft.EntityFrameworkCore;
 namespace Journal.Server.Database
 {
     /// <summary>
-    /// Контекст для подключения к базе данных.
-    /// Прежде чем начать использовать необходимо вызвать метод <see cref="SetConnectionString(string)"/> для установки строки подключения.
-    /// Данный экземпляр характеризует только одно подключение к базе данных. 
+    ///     Контекст для подключения к базе данных.
+    ///     Прежде чем начать использовать необходимо вызвать метод <see cref="SetConnectionString(string)"/> для установки строки подключения.
+    ///     Данный экземпляр характеризует только одно подключение к базе данных. 
     /// </summary>
     public class JournalDbContext : DbContext
     {
-                
+        #region public static
+        public static void SetConnectionString(string connectionString)
+            => _createJournalDbContext = () => new JournalDbContext(connectionString);
+
+        /// <summary>
+        ///     Создание контекста бд.
+        ///     Если до вызова этого метода не был вызван метод <see cref="SetConnectionString(string)"/>, то выбросит исключение <see cref="InvalidOperationException"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException" />
+        public static JournalDbContext CreateContext() => _createJournalDbContext();
+
+        private static Func<JournalDbContext> _createJournalDbContext
+            = () => throw new InvalidOperationException("Подключение к базе данных не настроено");
+        #endregion
+
         private readonly string _connectionString;
         public JournalDbContext(string connectionString) => _connectionString = connectionString;
 
@@ -36,30 +50,10 @@ namespace Journal.Server.Database
 
         public DbSet<Curriculum> Curriculums { get; set; }
 
-        
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
             base.OnConfiguring(optionsBuilder);
         }
-        
-        #region public static
-        
-        public static void SetConnectionString(string connectionString) 
-            => _createJournalDbContext = () => new JournalDbContext(connectionString); 
-        
-
-        private static Func<JournalDbContext> _createJournalDbContext 
-            = () =>  throw new InvalidOperationException("Подключение к базе данных не настроено");
-        
-        /// <summary>
-        ///     Создание контекста бд.
-        ///     Если до вызова этого метода не был вызван метод <see cref="SetConnectionString(string)"/>, то выбросит исключение <see cref="InvalidOperationException"/>
-        /// </summary>
-        /// <exception cref="InvalidOperationException" />
-        public static JournalDbContext CreateContext() => _createJournalDbContext();
-        
-        #endregion
     }
 }
